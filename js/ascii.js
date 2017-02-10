f19ad9be27a2644a422dd7733fa9a92f80751735
bulbil <n@nabilk.com>
2017-02-10 02:32:26 -0500

    adding customized ascii.js

---
+++ js/ascii.js
@@ -0,0 +1,168 @@
+// Author: Andrei Gheorghe (http://github.com/idevelop)
+
+var ascii = (function() {
+	function asciiFromCanvas(canvas, options) {
+		// Original code by Jacob Seidelin (http://www.nihilogic.dk/labs/jsascii/)
+		// Heavily modified by Andrei Gheorghe (http://github.com/idevelop)
+
+		var characters = (" .,:;i1tfLCG08@").split("");
+
+		var context = canvas.getContext("2d");
+
+		var canvasWidth = canvas.width;
+		var canvasHeight = canvas.height;
+		
+		var asciiCharacters = "";
+
+		// calculate contrast factor
+		// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
+		var contrastFactor = (259 * (options.contrast + 255)) / (255 * (259 - options.contrast));
+
+		var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+		for (var y = 0; y < canvasHeight; y += 2) { // every other row because letters are not square
+			for (var x = 0; x < canvasWidth; x++) {
+				// get each pixel's brightness and output corresponding character
+
+				var offset = (y * canvasWidth + x) * 4;
+
+				var color = getColorAtOffset(imageData.data, offset);
+	
+				// increase the contrast of the image so that the ASCII representation looks better
+				// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
+				var contrastedColor = {
+					red: bound(Math.floor((color.red - 128) * contrastFactor) + 128, [0, 255]),
+					green: bound(Math.floor((color.green - 128) * contrastFactor) + 128, [0, 255]),
+					blue: bound(Math.floor((color.blue - 128) * contrastFactor) + 128, [0, 255]),
+					alpha: color.alpha
+				};
+
+				// calculate pixel brightness
+				// http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+				var brightness = (0.299 * contrastedColor.red + 0.587 * contrastedColor.green + 0.114 * contrastedColor.blue) / 255;
+
+				var character = characters[(characters.length - 1) - Math.round(brightness * (characters.length - 1))];
+
+				asciiCharacters += character;
+			}
+
+			asciiCharacters += "\n";
+		}
+
+		options.callback(asciiCharacters);
+	}
+
+	// modified to return strings from each channel individually
+	function asciiChannelFromCanvas(canvas, options) {
+		// Original code by Jacob Seidelin (http://www.nihilogic.dk/labs/jsascii/)
+		// Heavily modified by Andrei Gheorghe (http://github.com/idevelop)
+
+		var characters = (" .,:;i1tfLCG08@").split("");
+
+		var context = canvas.getContext("2d");
+
+		var canvasWidth = canvas.width;
+		var canvasHeight = canvas.height;
+		
+		var asciiCharacters = "",
+			asciiStrings = [];
+
+		for(var i = 0; i < 6; i++){
+			asciiStrings.push("");
+		}
+
+		var perceptionShift = [0.299,0.587,0.114];
+
+		// calculate contrast factor
+		// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
+		var contrastFactor = (259 * (options.contrast + 255)) / (255 * (259 - options.contrast));
+
+		var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+		for (var y = 0; y < canvasHeight; y += 2) { // every other row because letters are not square
+			for (var x = 0; x < canvasWidth; x++) {
+				// get each pixel's brightness and output corresponding character
+
+				var offset = (y * canvasWidth + x) * 4;
+
+				var color = getColorAtOffset(imageData.data, offset);
+	
+				// increase the contrast of the image so that the ASCII representation looks better
+				// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
+				var contrastedColor = {
+					red: bound(Math.floor((color.red - 128) * contrastFactor) + 128, [0, 255]),
+					green: bound(Math.floor((color.green - 128) * contrastFactor) + 128, [0, 255]),
+					blue: bound(Math.floor((color.blue - 128) * contrastFactor) + 128, [0, 255]),
+					alpha: color.alpha
+				};
+
+				// calculate pixel brightness
+				// http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+				// var brightness = (0.299 * contrastedColor.red + 0.587 * contrastedColor.green + 0.114 * contrastedColor.blue) / 255;
+				
+				
+				function getCharacter(brightness){
+					var character = characters[(characters.length - 1) - Math.round(brightness * (characters.length - 1))];
+					return character;
+				}
+
+				var channels = ['red','green','blue'];
+
+				// for each channel generate a string representation based on the pure
+				// values and the corrected values
+				for ( var i = 0; i < asciiStrings.length; i++ ){
+					if (i < 3){
+						var brightness = (contrastedColor[channels[i%3]] * perceptionShift[i]) / 255;
+					} else {
+						var brightness = contrastedColor[channels[i%3]] / 255;
+					}
+					var char = getCharacter(brightness);
+					asciiStrings[i] += ( char === 'undefined') ? "" : char;
+				}
+
+			}
+
+			for ( var i = 0; i < asciiStrings.length; i++ ){
+				asciiStrings[i] += '\n';
+			}
+		}
+
+		options.callback(asciiStrings);
+	}
+
+	function getColorAtOffset(data, offset) {
+		return {
+			red: data[offset],
+			green: data[offset + 1],
+			blue: data[offset + 2],
+			alpha: data[offset + 3]
+		};
+	}
+
+	function bound(value, interval) {
+		return Math.max(interval[0], Math.min(interval[1], value));
+	}
+
+	return {
+		
+		fromCanvas: function(canvas, options) {
+
+			var doNothing;
+
+			options = options || {};
+			options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
+			options.callback = options.callback || doNothing;
+
+			return asciiFromCanvas(canvas, options);
+		},	
+
+		channelFromCanvas: function(canvas, options) {
+
+			var doNothing;
+
+			options = options || {};
+			options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
+			options.callback = options.callback || doNothing;
+
+			return asciiChannelFromCanvas(canvas, options);
+		}
+	};
+})();
